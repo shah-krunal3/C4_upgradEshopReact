5a6b8c9f93c00c83208a81d741b48a106c1f727d
Rameswary Behera <rameswarybehera@gmail.com>
2023-05-16 11:59:20 +0530

    First Commit- Navbar

---
+++ src/pages/AddProduct.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Common/Navbar/Layout';
+
+const AddProduct = () => {
+  return (
+    <Layout>
+      Add Product
+    </Layout>
+  )
+}
+
+export default AddProduct;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Common/Navbar/Layout'
+
+const Home = () => {
+  return (
+    <Layout>
+      Home
+    </Layout>
+  )
+}
+
+export default Home
