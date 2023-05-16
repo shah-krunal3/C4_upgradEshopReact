a08cdc5a71edd460ff6a2b6923826945728a6164
Rameswary Behera <rameswarybehera@gmail.com>
2023-05-16 11:55:09 +0530

    First Commit- Navbar

---
+++ src/Common/Navbar/Navbar.js
@@ -0,0 +1,115 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import "../../styles/Navbar.css";
+import { Button } from "@mui/material";
+
+const Search = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "31ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+  marginLeft: theme.spacing(2),
+}));
+
+const Navbar = () => {
+  return (
+    <div>
+      <Box>
+        <AppBar component={"nav"}>
+          <Toolbar>
+            <ShoppingCartIcon />
+            <Typography variant="h6" component={"div"} sx={{ flexgrow: 1 }}>
+              upGrad E-shop
+            </Typography>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            ></IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}
+            ></Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+            <Box sx={{ display: { xs: "none", sm: "flex" } }}>
+              <ul className="common_navbar">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/addproduct"}>Add Product</Link>
+                </li>
+                <Link to={"/login"}>
+                  <Button className="button" variant="contained">Login</Button>
+                  </Link>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box>
+        <Toolbar/>
+      </Box>
+    </div>
+  );
+};
+
+export default Navbar;
